f174e4a183b5adf4f982ee8b1c6d131a65af02c2
janthony09 <45113284+janthony09@users.noreply.github.com>
2018-12-01 11:38:22 +0800

    C

---
+++ HelloWorld2.java
@@ -0,0 +1,11 @@
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+            System.out.println("Hello World!");
+            System.out.println("I am Gabriel Aquino");
+            System.out.println("I am 19 yrs. old");
+            System.out.println("I am a Male");
+            System.out.println("The value of Pi is 3.1416");
+    }
+    
+}
